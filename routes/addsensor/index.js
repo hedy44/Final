636ecbe18336f945c020153194681fe012f9be4a
Hernani Diniz <hdiniz4@gmail.com>
2023-05-23 21:30:55 +0100

    GET method para obter locais no formul 'addsensor'

--- routes/addsensor/index.js
+++ routes/addsensor/index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const router = express.Router();
 const { sensor } = require('../../controllers');
 
-router.get('/', (req, res) => {
+// router.get('/', (req, res) => {
     
-    res.render('addsensor');
-});
+//     res.render('addsensor');
+// });
+
+router.get('/', async (req, res) => {
+    try {
+      const locals = await sensor.getLocals();
+      res.render('addsensor', { locals });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
+
+
 
 router.post('/', sensor.createSensor);
 router.delete('/', sensor.deleteSensor);
