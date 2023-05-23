0dead5f1b6ffc92a5dbf6a43bd6cbb4069bdc8df
Hernani Diniz <hdiniz4@gmail.com>
2023-05-23 21:29:33 +0100

    Associacao do model user ao model sensor

--- models/user.js
+++ models/user.js
@@ -1,16 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
 
-    const User = sequelize.define('user', 
-    {
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
-    },
-    {
-        freezeTableName: true //To not pluralize the table name
+  const User = sequelize.define('User', {
+      email: DataTypes.STRING,
+      
+      password: DataTypes.STRING
+  }, {
+      freezeTableName: true // Não pluralizar o nome da tabela
+  });
 
-     });
+  User.associate = (models) => {
+      User.hasMany(models.Sensor, {
+          foreignKey: 'userId',
+          as: 'sensors'
+      });
+  };
 
-     return User;
-
+  return User;
 };
-
