87eca3ae3a926ff6f3d25833f83f971e320040ce
Hernani-Diniz <hdiniz4@gmail.com>
2023-06-15 10:04:49 +0100

    delete controllers antigos

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-const controllers = {};
-
-controllers.user = require('./user');
-controllers.sensor = require('./sensor');
-controllers.locals = require('./locals');
-
-module.exports = controllers;
