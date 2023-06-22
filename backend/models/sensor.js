29af1ecb57b03d45adb6733a4035daad3a3ffbe7
Hernani-Diniz <hdiniz4@gmail.com>
2023-06-22 18:09:26 +0100

    chore: definir valor para model no form

--- backend/models/sensor.js
+++ backend/models/sensor.js
@@ -1,61 +1,47 @@
 module.exports = (sequelize, DataTypes) => {
-  const Sensor = sequelize.define('Sensor', {
-      sensortype: {
-          type: DataTypes.ENUM('Temp Sensor', 'Hum Sensor', 'Img Sensor'),
-          allowNull: false
-      },
-      model: {
-          type: DataTypes.ENUM('MKR WAN 1310', 'Camera', 'Other'),
-          allowNull: false
-      },
-      frequencyPlan: {
-          type: DataTypes.ENUM('Europe 863-870 MHZ (SF12 for RX2)', 'Europe 863-870 MHZ (SF9 for RX2)-recommended'),
-          allowNull: false
-      },
+    const Sensor = sequelize.define('Sensor', {
+       model: {
+         type: DataTypes.STRING(255),
+         allowNull: false,
+         defaultValue: 'Arduino MKR WAN 1310',
+          },
       devEUI: {
-          type: DataTypes.STRING(16),
-          allowNull: false
-      },
-      appEUI: {
-          type: DataTypes.STRING(16),
-          allowNull: false
+        type: DataTypes.STRING(16),
+        allowNull: false,
       },
       appKey: {
-          type: DataTypes.STRING(34),
-          allowNull: false
+        type: DataTypes.STRING(34),
+        allowNull: false,
       },
       sensorname: {
-          type: DataTypes.STRING(255),
-          allowNull: false
+        type: DataTypes.STRING(255),
+        allowNull: false,
       },
       location: DataTypes.STRING(255),
-
       localId: {
         type: DataTypes.INTEGER,
-        allowNull: true, // Permitir que seja nulo, pois pode ser um novo local
+        allowNull: true,
       },
-
       userId: {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
-
-      description: DataTypes.TEXT
-  }, {
-      freezeTableName: true // Não pluralizar o nome da tabela
-  });
-
-  Sensor.associate = (models) => {
+      description: DataTypes.TEXT,
+    }, {
+      freezeTableName: true,
+    });
+  
+    Sensor.associate = (models) => {
       Sensor.belongsTo(models.User, {
-          foreignKey: 'userId',
-          as: 'user'
+        foreignKey: 'userId',
+        as: 'user',
       });
-
+  
       Sensor.belongsTo(models.Locals, {
-          foreignKey: 'localId',
-          as: 'local'
+        foreignKey: 'localId',
+        as: 'local',
       });
+    };
+  
+    return Sensor;
   };
-
-  return Sensor;
-};
