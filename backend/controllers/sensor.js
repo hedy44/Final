8d5a2b0f6b6ea64bc4458dca30178ba6ccb28eab
Hernani-Diniz <hdiniz4@gmail.com>
2023-06-22 18:11:06 +0100

    chore: registo de dispositivo no ttn (basico)

--- backend/controllers/sensor.js
+++ backend/controllers/sensor.js
@@ -1,29 +1,24 @@
 const { models: { Sensor, Locals: Locals } } = require('../models');
-
-
+const mqttClient = require('../../backend/mqttconection/mqttConnector');
+const axios = require('axios');
 
 module.exports = {
-    createSensor: async (req, res) => {
-        try {
-          const {
-            sensortype,
-            model,
-            frequencyPlan,
-            devEUI,
-            appEUI,
-            appKey,
-            sensorname,
-            location,
-            localId,
-            description,
-          } = req.body;
+  createSensor: async (req, res) => {
+    try {
+      const {
+        devEUI,
+        appKey,
+        sensorname,
+        location,
+        localId,
+        description,
+      } = req.body;
 
-          const { id: userId } = req.user; // Obtém o userId do objeto req.user 
+      const { id: userId } = req.user;
 
-          let createdLocalId;
+      let createdLocalId;
 
       if (localId === 'new') {
-        // Cria um novo local
         const { newLocal, newLocalDescription } = req.body;
 
         const newLocalObj = await Locals.create({
@@ -37,55 +32,86 @@ module.exports = {
         createdLocalId = localId;
       }
 
-          const newSensor = await Sensor.create({
-            sensortype,
-            model,
-            frequencyPlan,
-            devEUI,
-            appEUI,
-            appKey,
-            sensorname,
-            location ,
-            localId :createdLocalId ,
-            description,
-            userId, // Atribui o ID do usuário ao criar o sensor
-          });
-          
-          return res.redirect('sensors');
-        } catch (error) {
-          console.error(error);
-          return res.status(500).json({ message: 'Internal Server Error' });
-        }
-      },  
+      const newSensor = await Sensor.create({
+        model: 'Arduino MKR WAN 1310',
+        devEUI,
+        appKey,
+        sensorname,
+        location,
+        localId: createdLocalId,
+        userId,
+        description,
+      });
 
-      //Metodo para ir buscar todos os sensores
-      getAllSensors: async (req, res) => {
-        try {
-          const sensors = await Sensor.findAll();
-          return sensors;
-        } catch (error) {
-          console.error(error);
-          return res.status(500).json({ message: 'Internal Server Error' });
-        }
-      },
+      // Registro do dispositivo na TTN
+      try {
+        const appID = 'projeto-2022-test'; // Substitua <app-id> pelo ID da sua aplicação na TTN
+        const ttnAPIUrl = `https://eu1.cloud.thethings.network/api/v3/applications/${appID}/devices`;
 
-//Metodo para apagar os sensores
-      deleteSensor: async (req, res) => {
+        const ttnPayload = {
+          "end_device": {
+            "ids": {
+              "device_id": sensorname,
+              "dev_eui": devEUI,
+              "join_eui": "0000000000000000"
+            },
+            "join_server_address": "eu1.cloud.thethings.network",
+            "network_server_address": "eu1.cloud.thethings.network",
+            "application_server_address": "eu1.cloud.thethings.network"
+          },
+          "field_mask": {
+            "paths": [
+              "join_server_address",
+              "network_server_address",
+              "application_server_address",
+              "ids.dev_eui",
+              "ids.join_eui"
+            ]
+        }
+};
+
+const headers = {
+  'Authorization': 'Bearer NNSXS.VBDNJAKZMO3PJCOUKZBSMMMQKXDNZJ2JIDAT25Q.LZL37FNTTUJURDKSYK4EM5I4O5AUPYKIZGM3R2I32BYEQLUS6HHA'
+};
+
+        await axios.post(ttnAPIUrl, ttnPayload ,{ headers });
+
+        console.log('Device registered on TTN');
+      } catch (error) {
+        console.error('Error registering device on TTN:', error);
+      }
+
+       
+      return res.redirect('sensors');
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  },
+
+  getAllSensors: async (req, res) => {
     try {
-      const {
-        id } = req.body;
+      const sensors = await Sensor.findAll();
+      return sensors;
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  },
+
+  deleteSensor: async (req, res) => {
+    try {
+      const { id } = req.body;
       await Sensor.destroy({
         where: { id: id }
-      })
+      });
       return res.status(200).json({ message: 'deleted' });
-
     } catch (error) {
       console.error(error);
       return res.status(500).json({ message: 'Internal Server Error when trying to delete' });
     }
   },
 
-  //buscar os locais
   getLocals: async (req, res) => {
     try {
       const locals = await Locals.findAll();
@@ -98,10 +124,10 @@ module.exports = {
 
   getUserSensors: async (req, res) => {
     try {
-      const userId = req.user.id; // Obtém o ID do usuário logado
+      const userId = req.user.id;
 
       const sensors = await Sensor.findAll({
-        where: { userId }, // Filtra pelos sensores associados ao usuário logado
+        where: { userId },
       });
 
       return sensors;
@@ -110,5 +136,4 @@ module.exports = {
       return res.status(500).json({ message: 'Internal Server Error' });
     }
   },
-
-}
+};
