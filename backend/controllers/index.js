f80a583ef2719b365babf72b8d559f4aec1442fe
Hernani-Diniz <hdiniz4@gmail.com>
2023-06-27 20:34:04 +0100

    chore: import do controller readings no index

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -3,5 +3,7 @@ const controllers = {};
 controllers.user = require('./user');
 controllers.sensor = require('./sensor');
 controllers.locals = require('./locals');
+controllers.readings = require('./readingsController');
+
 
 module.exports = controllers;
