78746044f5ccfedf048d92b406869474efb6ab39
Hernani-Diniz <hdiniz4@gmail.com>
2023-06-27 20:40:39 +0100

    chore:  adiçao da funçao Savereadings

--- backend/mqttconection/mqttConnector.js
+++ backend/mqttconection/mqttConnector.js
@@ -1,5 +1,9 @@
 const mqtt = require('mqtt');
 const io = require('socket.io')(4000);
+const db = require('../models');
+const Sensor = db.models.Sensor;
+const readingsController = require('../controllers/readingsController');
+
 
 
 const options = {
@@ -15,49 +19,79 @@ const options = {
 
 const client = mqtt.connect('mqtt://eu1.cloud.thethings.network:1883', options);
 
-client.on('connect', function() {
-    console.log('Client connected to TTN');
-    // client.subscribe('0000000000000000' + '/devices/+/up');
-    client.subscribe('#');
-  });
-
-  let temperature; 
-  let humidity;
-
-  client.on('message', function(topic, message) {
-    const msg = JSON.parse(message);
-    const deviceId = msg.end_device_ids.device_id;
-    const payloadFields = msg.uplink_message.decoded_payload;
-
-    if (payloadFields.temp_c) {
-        temperature = payloadFields.temp_c;
-        console.log('Temperature:', temperature);
-        // Faça o que desejar com a temperatura recebida, como salvar em um banco de dados ou processá-la de alguma forma.
-      }
-    
-    if (payloadFields.humidity) {
-       humidity = payloadFields.humidity;
-        console.log('Humidity:', humidity);
-        // Faça o que desejar com a umidade recebida, como salvar em um banco de dados ou processá-la de alguma forma.
-      }
-//TODO: criar para a base de dados
-      // Emita os dados para o cliente conectado via socket.io
-  io.emit('temperature', temperature);
-  io.emit('humidity', humidity);
+client.on('connect', function () {
+  console.log('Client connected to TTN');
+  client.subscribe('#');
 });
 
-io.on('connection', function(socket) {
+client.on('message', async function (topic, message) {
+  await handleMessage(topic, message);
+});
+
+async function handleMessage(topic, message) {
+  const msg = JSON.parse(message);
+  const payloadFields = msg.uplink_message.decoded_payload;
+
+  let temperature = null;
+  let humidity = null;
+  let userId = null;
+
+  if (payloadFields.temp_c) {
+    temperature = payloadFields.temp_c;
+    console.log('Temperature:', temperature);
+  }
+
+  if (payloadFields.humidity) {
+    humidity = payloadFields.humidity;
+    console.log('Humidity:', humidity);
+  }
+
+  // 1. Extrair o device_id do payload
+    const device_id = msg.end_device_ids.device_id;;
+    console.log('Device ID:', device_id);
+
+    // 2. Encontrar o sensor com base no device_id
+try {
+  const sensor = await Sensor.findOne({ where: { sensorname: device_id } });
+  if (sensor) {
+    userId = sensor.userId;
+  } else {
+    throw new Error('Sensor not found');
+  }
+} catch (error) {
+  console.error('Error retrieving sensor:', error);
+  return;
+}
+  // 3. Crie um objeto com as informações da leitura
+  const sensorDataPayload = {
+    temperature,
+    humidity,
+    sensorId: device_id,
+    userId
+  };
+
+  // 4. Salvar a leitura utilizando o controlador readingsController
+  try {
+    await readingsController.saveReading(sensorDataPayload.temperature, sensorDataPayload.humidity, sensorDataPayload.sensorId);
+  } catch (error) {
+    console.error('Error saving sensor data:', error);
+    return;
+  }
+  // 5. Emita os eventos para o cliente
+  io.emit('temperature', temperature);
+  io.emit('humidity', humidity);
+}
+
+io.on('connection', function (socket) {
   console.log('Client connected: ' + socket.id);
 
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function () {
     console.log(socket.id + ' disconnected');
   });
 });
-    
 
 client.on('error', function (error) {
-  console.error('Erro na conexão MQTT:', error);
+  console.error('MQTT connection error:', error);
 });
 
-// Exporte o objeto do cliente MQTT
 module.exports = client;
