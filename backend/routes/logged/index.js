4dc2ab34290336a1117d613044cd2e01db722710
Hernani-Diniz <hdiniz4@gmail.com>
2023-07-16 22:38:22 +0100

    chore:ver sensrs adicionados no mapa na paginicial

--- backend/routes/logged/index.js
+++ backend/routes/logged/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
-//const { user } = require('../../controllers');
+const { sensor } = require('../../controllers');
 const authMiddleware = require('../../middlewares/auth.js');
 
-router.get('/',authMiddleware, (req, res) => {
-   
-    
-        res.render('logged',{email: req.user.email});
-    
+router.get('/', authMiddleware, async (req, res) => {
+  try {
+    // Obtenha os dados dos sensores do banco de dados usando o controller
+    const userSensors = await sensor.getUserSensors(req, res);
+
+    // Renderize a página "logged.hbs" passando os dados dos sensores e o email do usuário
+    res.render('logged', { email: req.user.email, sensors: userSensors });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
