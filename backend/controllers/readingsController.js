9ca307856ba66e64217f16569ff7f2c68b99846d
Hernani-Diniz <hdiniz4@gmail.com>
2023-07-16 22:34:23 +0100

    chore: Tentativa de metodo para obter tempMaxeMin

--- backend/controllers/readingsController.js
+++ backend/controllers/readingsController.js
@@ -58,6 +58,35 @@ readingsController.getSensorReadings = async (req, res) => {
     }
 };
 
+readingsController.getTemperatureRange = async (req, res) => {
+  try {
+    const sensorname = req.params.sensorname;
+
+    // Obtenha a temperatura máxima e mínima do sensor com base no sensorname
+    const temperatures = await db.models.SensorData.findAll({
+      where: { sensorname },
+      attributes: [
+        [db.sequelize.fn('max', db.sequelize.col('temperature')), 'maxTemperature'],
+        [db.sequelize.fn('min', db.sequelize.col('temperature')), 'minTemperature']
+      ]
+    });
+
+    if (temperatures.length > 0) {
+      const maxTemperature = temperatures[0].dataValues.maxTemperature;
+      const minTemperature = temperatures[0].dataValues.minTemperature;
+
+      // Renderize a página logged.hbs passando as temperaturas máxima e mínima
+      return res.render('logged', { email: req.user.email, maxTemperature, minTemperature });
+    } else {
+      console.log('Temperaturas não encontradas.');
+      return res.status(404).json({ message: 'Temperaturas não encontradas' });
+    }
+  } catch (error) {
+    console.error('Erro ao buscar temperaturas:', error);
+    return res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+};
+
 
 
 module.exports = readingsController;
