689cba1368a0d2ebadd6f5e38b127e75969c5b6d
Hernani-Diniz <hdiniz4@gmail.com>
2023-07-16 22:37:31 +0100

    chore: rota para graficos

--- backend/routes/graphs/index.js
+++ backend/routes/graphs/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { user } = require('../../controllers');
+const { sensor } = require('../../controllers');
 const authMiddleware = require('../../middlewares/auth.js');
 
 router.get('/',authMiddleware, (req, res) => {
     
     res.render('graphs');
 });
+
+router.get('/:sensorname', sensor.showSensorGraphs);
 
 module.exports = router;
