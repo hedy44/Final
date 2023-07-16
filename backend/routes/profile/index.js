48350f5bf5443b572788c6c6ed46f7892eaa65f3
Hernani-Diniz <hdiniz4@gmail.com>
2023-07-16 22:38:37 +0100

    chore: Dados do profile

--- backend/routes/profile/index.js
+++ backend/routes/profile/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../../middlewares/auth.js');
 
+
 router.get('/',authMiddleware, (req, res) => {
     
    
-        res.render('profile', {email: req.user.email});
+        res.render('profile', {email: req.user.email,
+                                gender: req.user.gender,
+                                username: req.user.username,
+                                firstName: req.user.firstName,
+                                lastName: req.user.lastName,
+                                age: req.user.age});
     
 });
+
 
 module.exports = router;
