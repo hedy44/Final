e7dae6c6593a7b09e1bc079c4fa3a7445d59ac9d
Hernani-Diniz <hdiniz4@gmail.com>
2023-07-16 22:36:30 +0100

    chore: melhorar o perfil e atribuir imagem a user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,60 +1,70 @@
 const jwt = require('jsonwebtoken');
-const { models : { User }} = require('../models'); //db.models.User = require('./user')(sequelize, Sequelize.DataTypes) do index do model
+const { models: { User } } = require('../models');
 
+module.exports = {
+  create: async (req, res) => {
+    const { email, password, username, firstName, lastName, age, gender } = req.body;
 
- module.exports = {
-     create: async (req,res) => {
-        const { email, password } = req.body;
+    try {
+      // Verificar se o usuário com o email já existe
+      const existingUser = await User.findOne({ where: { email } });
 
-        try {
-          // Check if user with email already exists
-          const existingUser = await User.findOne({ 
-            where: {email} });
+      if (existingUser) {
+        return res.render("register", {
+          msg: "Email ID already taken",
+          msg_type: "error",
+        });
+      }
 
-          if (existingUser) {
-            return res.render("register", {
-                msg: "Email ID already Taken",
-                msg_type: "error",
-              });
-          }
-          // Create a new user
-          const newUser = await User.create({ email, password });
+      // Definir a foto de acordo com o gênero selecionado
+      let photo = '';
+      if (gender === 'male') {
+        photo = 'male_avatar.png';
+      } else if (gender === 'female') {
+        photo = 'female_avatar.png';
+      }
 
-          // Generate JWT token
-          const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-           // Set the token as a cookie
-          res.cookie('jwt', token, { secure: true });
+      // Criar um novo usuário
+      const newUser = await User.create({ email, password, username, firstName, lastName, age, gender });
 
-          res.render('login');
-        } catch (error) {
-          console.error(error);
-          res.status(500).send('Internal Server Error');
-        }
- },
+      // Gerar o token JWT
+      const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-    login: async (req,res) => {
-      
-      if (req.body.email && req.body.password) {
-        const { email, password } = req.body;
-  
-        let user = await User.findOne({ where: { email, password } });
-  
+      // Definir o token como um cookie
+      res.cookie('jwt', token, { secure: true });
+
+      res.render('login');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  },
+
+  login: async (req, res) => {
+    if (req.body.email && req.body.password) {
+      const { email, password } = req.body;
+
+      try {
+        const user = await User.findOne({ where: { email, password } });
+
         if (user) {
-          // Generate token
+          // Gerar token
           const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-  
-          // Set the token as a cookie
+
+          // Definir o token como um cookie
           res.cookie('jwt', token, { httpOnly: true });
-  
-          res.render('profile', { email });
+
+          res.render('profile', { email, gender: user.gender });
         } else {
-          res.render('login',{
-            msg: "Email or password don't match ",
+          res.render('login', {
+            msg: "Email or password don't match",
             msg_type: "error",
           });
         }
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
       }
-        
-}
-  
-}
+    }
+  }
+};
