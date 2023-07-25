543c4f559ffa9deb42be9d72debd30ccff0625c6
Hernani-Diniz <hdiniz4@gmail.com>
2023-07-25 22:28:23 +0100

    chore: ver so locais do user

--- backend/routes/addsensor/index.js
+++ backend/routes/addsensor/index.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { sensor } = require('../../controllers');
+const { sensor, locals } = require('../../controllers'); // Importar também o controlador 'local'
 const authMiddleware = require('../../middlewares/auth');
 
-router.get('/',authMiddleware, async (req, res) => {
-    try {
-      const locals = await sensor.getLocals();
-      res.render('addsensor', { locals });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
-
-
+router.get('/', authMiddleware, async (req, res) => {
+  try {
+    const userLocals = await locals.getAllLocals(req.user.id); // Buscar apenas os locais do usuário logado
+    res.render('addsensor', { locals: userLocals }); // Renomear a variável para "userLocals"
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 
 router.post('/', authMiddleware, sensor.createSensor);
-router.delete('/',authMiddleware,  sensor.deleteSensor);
-
+router.delete('/', authMiddleware, sensor.deleteSensor);
 
 module.exports = router;
